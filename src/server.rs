d66ee10a3e55a128a27a4a3077298937ca700a50
Francesco Ruta <francescoruta.99@gmail.com>
2024-04-03 12:48:27 +0200

    push_connection_async

--- src/server.rs
+++ src/server.rs
@@ -80,6 +80,18 @@ impl<Connection> RunningServer<Connection> {
         self.accept_connection_tx.send(conn).await
     }
     
+    pub async fn push_connection_async<F: std::future::Future<Output = Option<Connection>> + Send + 'static>(&self, conn: (u32, F), callback: impl FnOnce(Option<Result<(), tokio::sync::mpsc::error::SendError<(u32, Connection)>>>) + Send + 'static) where Connection: Send + 'static {
+        let accept_connection_tx = self.accept_connection_tx.clone();
+        tokio::spawn(async move {
+            let (id, conn) = conn;
+            if let Some(conn) = conn.await {
+                callback(Some(accept_connection_tx.send((id, conn)).await));
+            } else {
+                callback(None);
+            }
+        });
+    }
+    
     pub fn get_local_connection_factory(&self) -> LocalConnectionFactory {
         LocalConnectionFactory {
             connections: Arc::clone(&self.connections),
